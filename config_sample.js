7beba639922c435f0a10ca26eab18bc73e308054
mikefab <mikefabrikant@gmail.com>
2017-05-28 20:23:39 -0400

    Config sample

---
+++ config_sample.js
@@ -0,0 +1,18 @@
+module.exports = {
+  raw_dir: '../../mnt/zika/paho/raw/',
+  json_dir: '../../mnt/zika/paho/json/',
+  index: {
+    B: 'autochthonous_cases_suspected',
+    C: 'autochthonous_cases_confirmed',
+    D: 'imported_cases',
+    E: 'incidence_rate',
+    F: 'deaths',
+    G: 'confirmed_congenital',
+    H: 'population_x_1k',
+    I: 'congenital_suspected',
+    J: 'congenital_probable',
+    K: 'gbs_total',
+    L: 'gbs_confirmed'
+  }
+
+}
