a2bcd6552550df9351efae50d4ae7165c0b79f9f
mikefab <mikefabrikant@gmail.com>
2017-06-08 15:57:58 -0400

    Only save new files

--- fetch_cases.js
+++ fetch_cases.js
@@ -8,20 +8,24 @@ var request = require('request');
 var XLSX = require('xlsx');
 
 request('http://www.paho.org/hq/index.php?option=com_content&view=article&id=12390&Itemid=42090&lang=en', function (error, response, body) {
- var urls = getHrefs(body).filter(l => {
-   return l.match('doc_view')
- })
- bluebird.each(urls, url => {
-   console.log(url)
-   return fetch_doc(url);
- }, {concurrency: 1})
- .then(() => {
-   console.log('Done!');
-   process.exit();
- })
+
+   var urls = getHrefs(body).filter(l => {
+     return l.match('doc_view')
+   })
+
+  fs.readdir(config.raw_dir, (err, files) => {
+    bluebird.each(urls, url => {
+      console.log(url)
+      return fetch_doc(url, files);
+    }, {concurrency: 1})
+    .then(() => {
+      console.log('Done!');
+      process.exit();
+    })  });
+
 });
 
-function fetch_doc(url) {
+function fetch_doc(url, files) {
   return new Promise((resolve, reject) => {
     var options = {
       url: base_url + url,
@@ -30,6 +34,7 @@ function fetch_doc(url) {
       },
       encoding: null
     };
+
     request.get(options, (err, res, body) => {
       var title = res.headers['content-disposition'].match(/filename="(.+?)";/)[1];
       if (title.match(/xls$/)) {
@@ -42,10 +47,20 @@ function fetch_doc(url) {
         var bstr = arr.join("");
         var workbook = XLSX.read(bstr, { type: "binary" });
         var worksheet = workbook.Sheets['2015 ENG'];
-        fs.writeFile(config.raw_dir + title.replace('xls', 'json'), JSON.stringify(worksheet), (err, response) => {
-          return resolve();
-        });
+        var file = title.replace('xls', 'json');
+        if (files.indexOf(file) === -1) {
+          // This file has not been downloaded before
+          console.log('download new', file)
+          fs.writeFile(config.raw_dir + title.replace('xls', 'json'), JSON.stringify(worksheet), (err, response) => {
+            return resolve();
+          });
+        } else {
+          console.log('already have', file)
+          // Already have file, move along.
+          resolve();
+        }
       } else {
+        // Not an xls file, likely pdf.
         return resolve();
       }
     });
