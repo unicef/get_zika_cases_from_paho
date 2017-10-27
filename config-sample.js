fdea3039cb8b4dbf9f63f351f0a4a2619c62484f
mikefab <mikefabrikant@gmail.com>
2017-10-27 16:52:25 -0400

    Add config-sample

---
+++ config-sample.js
@@ -0,0 +1,18 @@
+module.exports = {
+  dir_iso: '../../mnt/zika/paho/iso/',
+  dir_raw: '../../mnt/zika/paho/raw/',
+  dir_epi: '../../mnt/zika/paho/epi/',
+  index: {
+    B: 'autochthonous_cases_suspected',
+    C: 'autochthonous_cases_confirmed',
+    D: 'imported_cases',
+    E: 'incidence_rate',
+    F: 'deaths',
+    G: 'confirmed_congenital',
+    H: 'population_x_1k',
+    I: 'congenital_suspected',
+    J: 'congenital_probable',
+    K: 'gbs_total',
+    L: 'gbs_confirmed'
+  }
+}
